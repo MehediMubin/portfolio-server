757ce206d67d23c15d8f33fe03076eb05e529e27
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-30 22:09:24 +0600

    minor modfication on project service

--- src/modules/Project/project.service.ts
+++ src/modules/Project/project.service.ts
@@ -7,9 +7,19 @@ const createProject = async (payload: TProject) => {
 };
 
 const updateProject = async (id: string, payload: Partial<TProject>) => {
+  const existingProject = await ProjectModel.findById(id);
+  if (!existingProject) {
+    throw new Error("Project not found");
+  }
+
   const result = await ProjectModel.findOneAndUpdate({ _id: id }, payload, {
     new: true,
   });
+
+  if (!result) {
+    throw new Error("Update failed");
+  }
+
   return result;
 };
 
