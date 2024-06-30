01c2e111aa32e440f999618bf7c89461840bdba2
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-30 16:06:47 +0600

    created not found route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 // import globalErrorHandler from './app/middlewares/globalErrorhandler';
-// import notFound from './app/middlewares/notFound';
+import notFound from './middlewares/notFound';
 // import router from './app/routes';
 
 const app: Application = express();
@@ -21,6 +21,6 @@ app.get("/", (req: Request, res: Response) => {
 // app.use(globalErrorHandler);
 
 //Not Found
-// app.use(notFound);
+app.use(notFound);
 
 export default app;
