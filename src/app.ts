3e50c5698e49bd2b7feb29e77377c66b1e58f840
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-07-06 19:50:44 +0600

    update: cors origin

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: "https://hopeful-turing-2346d6.netlify.app",
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use("/api", router);
